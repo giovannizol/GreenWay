e5895f97219890d39200a468c3b0a2326950bee0
giovanni.zol <giovanni.zol.24@stud.itsaltoadriatico.it>
2026-06-05 11:02:07 +0200

    docs: aggiornato README

--- clearwaytech/src/layouts/MainLayout/MainLayout.jsx
+++ clearwaytech/src/layouts/MainLayout/MainLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Sidebar } from "../components/Sidebar/Sidebar";
-import { Header } from "../components/Header/Header";
-import { NotificationPanel } from "../components/NotificationPanel/NotificationPanel";
+import { Sidebar } from "../../components/Sidebar/Sidebar";
+import { Header } from "../../components/Header/Header";
+import { NotificationPanel } from "../../components/NotificationPanel/NotificationPanel";
 import "./MainLayout.css";
 
 export function MainLayout({ section, onSelectSection, children }) {
